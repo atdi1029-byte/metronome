327ffe87c9eb639295f0b5353a1c721bff55cced
Alex Barnett <alexbarnett@mac.mynetworksettings.com>
2026-05-06 19:35:21 -0400

    Fix PWA paths for GitHub Pages subdirectory

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,27 @@
-const CACHE = 'metronome-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'metronome-v2';
+const FILES = [
+  '/metronome/',
+  '/metronome/index.html',
+  '/metronome/manifest.json',
+  '/metronome/service-worker.js',
+  '/metronome/icon-192.png',
+  '/metronome/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
